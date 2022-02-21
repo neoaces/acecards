f780a5851cba6ec48da5df028b381a185b35ddbb
anzle <anzle@Andys-MBA.local>
2022-02-21 17:24:25 -0500

    enum Variant to allow for different cards

--- ace/src/lib.rs
+++ ace/src/lib.rs
@@ -1,26 +1,46 @@
 use chrono::prelude::*;
 
 #[derive(Debug)]
+enum Variant {
+    Simple,
+    Extended,
+}
+
+#[derive(Debug)]
 pub struct Card {
-    question: String,
-    answer: String,
+    pub question: String,
+    pub answer: String,
     timestamp: DateTime<Local>,
+    variant: Variant,
 }
 
 impl Card {
-    pub fn simple(question: &str, answer: &str) -> Card {
+    pub fn makeCard(question: &str, answer: &str) -> Card {
         let dt: DateTime<Local> = chrono::Local::now();
-
 
         Card {
             question: question.to_string(),
             answer: answer.to_string(),
             timestamp: dt,
+            variant: Variant::Simple,
         }
-    }
-
-    pub fn print_card(&self) {
-        println!("{} answer {}", self.question, self.timestamp.format("%Y-%m-%d %R %Z"))
     }
 }
 
+impl Card {
+    pub fn print_card(&self) {
+        println!(
+            "{:?} answer {}",
+            self.variant,
+            self.timestamp.format("%Y-%m-%d %R %Z")
+        )
+    }
+
+    pub fn print_ans(&self) {
+        println!("{}", self.answer)
+    }
+
+    pub fn print_q(&self) {
+        println!("{}", self.question)
+    }
+}

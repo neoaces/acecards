2cbd50042b7bdbd6ff80f33da6c3509f82f8a463
anzle <anzle@Andys-MBA.local>
2022-02-21 17:38:22 -0500

    amended false function

--- acespace/src/main.rs
+++ acespace/src/main.rs
@@ -1,7 +1,7 @@
 use ace;
 
 fn main() {
-    let new: ace::Card = ace::Card::simple("Who is the president?", "obama");
+    let new: ace::Card = ace::Card::makeCard("Who is the president?", "obama");
 
     println!("{:#?}", new);
     new.print_card();
